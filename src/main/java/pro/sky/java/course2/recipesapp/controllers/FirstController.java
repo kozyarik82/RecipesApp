b9a0b26d89e39320e8ac6209d58b3a269b2a7701
kozyar <kozyarik@gmail.com>
2022-12-12 01:58:50 +0100

    первый коммит

---
+++ src/main/java/pro/sky/java/course2/recipesapp/controllers/FirstController.java
@@ -0,0 +1,4 @@
+package pro.sky.java.course2.recipesapp.controllers;
+
+public class FirstController {
+}
---
+++ src/test/java/pro/sky/java/course2/recipesapp/RecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.recipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
