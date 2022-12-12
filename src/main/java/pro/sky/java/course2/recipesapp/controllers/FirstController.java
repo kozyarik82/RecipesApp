c2d33bed883aa8ae232fc6863c6faa16b7e3b328
kozyar <kozyarik@gmail.com>
2022-12-12 02:54:22 +0100

    решение задачи

--- src/main/java/pro/sky/java/course2/recipesapp/controllers/FirstController.java
+++ src/main/java/pro/sky/java/course2/recipesapp/controllers/FirstController.java
@@ -1,4 +1,18 @@
 package pro.sky.java.course2.recipesapp.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FirstController {
+    @GetMapping
+    public String hello() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String info(@RequestParam String info) {
+        return "Info: имя разработчика: Kozyar Oxana, название проекта: Recipes, дата создания 12.12.2022, опимание: приложение предназначено для создания рецептов" + info;
+    }
 }
